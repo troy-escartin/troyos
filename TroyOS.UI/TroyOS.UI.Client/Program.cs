4abbc6f47e3dca1ca80493ff4787375632db9e5d
Troy Kristofer Escartin <Troy.Escartin@bakermckenzie.com>
2026-08-18 11:30:17 +0800

    completed and ready for deployment

--- TroyOS.UI/TroyOS.UI.Client/Program.cs
+++ TroyOS.UI/TroyOS.UI.Client/Program.cs
@@ -1,5 +1,10 @@
+using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using TroyOS.UI.Client;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
 
 await builder.Build().RunAsync();
